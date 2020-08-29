e483dd47eca079e29761970f0d43e3a2f9a785e1
Devjyoti Ghosh <devjyotighosh02@gmail.com>
2020-08-30 03:26:21 +0530

    Added Infinite Scrolling

--- src/components/sections/QuestionList.tsx
+++ src/components/sections/QuestionList.tsx
@@ -1,9 +1,9 @@
-import React from "react";
+import React, { useRef } from "react";
 import { useInfiniteQuery } from "react-query";
-import { ReactQueryDevtools } from "react-query-devtools";
-import { Stack } from "@chakra-ui/core";
+import { Box, Stack, Spinner, Text } from "@chakra-ui/core";
 import { getStackQuestions } from "../../api/stackOverflowApi";
 import { Item, PaginatedApiResponse } from "../../api/stackOverflowApiType";
+import useIntersectionObserver from "../../hooks/useIntersectionObserver";
 import BoxWithModal from "../ui/BoxWithModal";
 
 //Added types for the results of useInfiniteQuery
@@ -28,37 +28,61 @@ export default function QuestionList() {
     error,
     fetchMore,
     canFetchMore,
+    isFetching,
   }: InfiniteQuery = useInfiniteQuery("questions", getStackQuestions, {
-    getFetchMore: (lastGroup, allGroups) => lastGroup.nextPageNumber,
+    getFetchMore: (lastGroup) => lastGroup.nextPageNumber,
     staleTime: 1000 * 60 * 60 * 24 * 365,
   });
-  //Shows error or loading and the resutant list if api is successfull
-  if (status === "error") {
-    return <span>Error: {error?.message}</span>;
-  }
-  if (status === "loading") {
-    return <span>Loading...</span>;
-  }
+
+  const spinnerDiv = useRef<HTMLElement>(null);
+
+  // This hook takes in the following
+  // target ref,
+  // onIntersect:  function to be called when the ref is on sreen
+  //  enabled: only calls the function if there are more Items to be fetched
+  useIntersectionObserver({
+    target: spinnerDiv,
+    onIntersect: fetchMore,
+    enabled: canFetchMore,
+  });
 
   return (
-    <Stack spacing={8}>
-      <>
-        {/* The data from the api calls are stored in an array */}
-        {data?.map((page: any, i: any) => (
-          <React.Fragment key={i}>
-            {/* Displays result of 1 api call */}
-            {page?.data?.items?.map((item: Item) => (
-              <BoxWithModal
-                question={item.title}
-                date={item.creation_date.toString()}
-                author={item.owner.display_name}
-                link={item.link}
-              />
+    <>
+      {status === "error" ? (
+        <Text>{error?.message}</Text>
+      ) : (
+        <Stack spacing={8}>
+          <>
+            {/* The data from the api calls are stored in an array */}
+            {data?.map((page: any, i: any) => (
+              <React.Fragment key={i}>
+                {/* Displays result of 1 api call */}
+                {page?.data?.items?.map((item: Item) => (
+                  <BoxWithModal
+                    question={item.title}
+                    date={item.creation_date.toString()}
+                    author={item.owner.display_name}
+                    link={item.link}
+                  />
+                ))}
+              </React.Fragment>
             ))}
-          </React.Fragment>
-        ))}
-      </>
-      <ReactQueryDevtools initialIsOpen={false} />
-    </Stack>
+            {/* Spinner button only shows during intial load and before all pages are loaded */}
+            {canFetchMore || isFetching ? (
+              <Box
+                display="flex"
+                alignItems="center"
+                justifyContent="center"
+                pb={5}
+                mb={5}
+                ref={spinnerDiv}
+              >
+                <Spinner size="xl" color="blue.500" />
+              </Box>
+            ) : null}
+          </>
+        </Stack>
+      )}
+    </>
   );
 }
---
+++ src/hooks/useIntersectionObserver.tsx
@@ -0,0 +1,49 @@
+import React, { useEffect } from "react";
+
+interface IntersectionObserver {
+  root?: any;
+  target?: React.MutableRefObject<HTMLElement | null>;
+  onIntersect: any;
+  enabled?: boolean | undefined;
+  threshold?: number;
+  rootMargin?: string;
+}
+
+// useIntersectionObserver checks if the target ref is intersecting with the device viewport or the root element
+// if it is it calls the onIntersect function
+export default function useIntersectionObserver({
+  root,
+  target,
+  onIntersect,
+  threshold = 1.0,
+  rootMargin = "0px",
+  enabled = true,
+}: IntersectionObserver) {
+  useEffect(() => {
+    if (!enabled) {
+      return;
+    }
+
+    const observer = new IntersectionObserver(
+      (entries) =>
+        entries.forEach((entry) => entry.isIntersecting && onIntersect()),
+      {
+        root: root && root.current,
+        rootMargin,
+        threshold,
+      }
+    );
+
+    const el = target && target.current;
+
+    if (!el) {
+      return;
+    }
+
+    observer.observe(el);
+
+    return () => {
+      observer.unobserve(el);
+    };
+  }, [enabled, root, rootMargin, threshold, target, onIntersect]);
+}
