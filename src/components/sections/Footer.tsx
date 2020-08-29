9713eb5dbdaa5bffd5085d6272e97ad131cb6038
Devjyoti Ghosh <devjyotighosh02@gmail.com>
2020-08-29 20:49:54 +0530

    Updated Footer ui

--- src/components/sections/Footer.tsx
+++ src/components/sections/Footer.tsx
@@ -1,7 +1,8 @@
 import React from "react";
-import { Flex, Text } from "@chakra-ui/core";
+import { Flex, Text, useColorModeValue } from "@chakra-ui/core";
 
 export default function Footer() {
+  const bgColor = useColorModeValue("gray.50", "gray.900");
   return (
     <Flex
       direction="row"
@@ -10,6 +11,7 @@ export default function Footer() {
       bottom="0"
       width="100%"
       py={4}
+      bg={bgColor}
     >
       <Text>Stackoverflow Questions List Created by Devjyoti Ghosh</Text>
     </Flex>
